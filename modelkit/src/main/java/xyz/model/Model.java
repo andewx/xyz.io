7b16de5ab2526b257c3b85198294d268428f1803
SAL ANDERSON <brian.michael.anderson@gmail.com>
2021-02-08 09:46:24 +0900

    Developed Model Kit More

--- modelkit/src/main/java/xyz/model/Model.java
+++ modelkit/src/main/java/xyz/model/Model.java
@@ -1,20 +1,37 @@
 package xyz.model;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 
-//Model Interface - Database/Key Store Mapping Interface K is the Key Store for the Resource ID
-//Relational models do not store references to other models in there bodies unless through ref properties.
-//Models are primarily database objects
+
+/**
+ * xyz.Model.Model interface
+ * <p>Application Data Model. Interfaces presents models as Key:Value Property Lists with internal
+ * model nodes available for tree structures. Presents relational database specifiers like Unique ID, Parent ID,
+ and a model Name for unique database identification.* </p>
+ */
+
 public interface Model {
 
-    public Model createModelDBInstance(); //Returns this model with appropriate IDs and relational position in db
+    //Model Factory Methods
+    public Model createModel(String unique); //Initialization basis for a model
+
+    //Model ID Types
+    public String getUID(); //Gets Model Id
     public String getName(); //Model Class Name
-    public boolean isUnique(); //Is Model Duplicated
-    public String getModelID();  //Model ID For Rel JSON DB
-    public String getParentID(); //Parent ID for Rel Items
-    public HashMap<String,String> getProperties(); //Properties List
-    public HashMap<String,String> hasModelsWithEdges(); //Directive Relational Edges Have Properties {Model : EdgeTye}
-    public Model parseJson(String json); //Constructs Model from JSON
-    public String encodeJson(); //Encodes Model into JSON Object
+
+    //Model Property Methods
+    public void setProperty(String propKey, String propValue); //This may mirror internal attributes (reflection case)
+    public String getProperty(String propKey, String propValue); //Get Property Key Value
+    public String getPropertyType(String propKey); //Switch case method for casting types
+
+    //Model Internal Recursion (K - Tree)
+    public ArrayList<Model> hasModels(); //Nodal References to other Model Objects
+    public void addModel(Model m);
+    public Model getModel(String uid);
+
+    //Model Property Reflection
+    public void syncModel(boolean syncInternal); //Synchronize Property Lists And Model Lists Recursively (cycle risk)
 
 }
+
