9ba4ecef0bdface24635963b5798f760665506c9
SAL ANDERSON <brian.michael.anderson@gmail.com>
2021-02-09 09:52:44 +0900

    Master Commit - Removed unused default modules deleted files

--- app/src/main/java/xyz/app/App.java
+++ app/src/main/java/xyz/app/App.java
@@ -3,19 +3,10 @@
  */
 package xyz.app;
 
-import xyz.list.LinkedList;
 
-import static xyz.utilities.StringUtils.join;
-import static xyz.utilities.StringUtils.split;
-import static xyz.app.MessageUtils.getMessage;
-
-import org.apache.commons.text.WordUtils;
 
 public class App {
     public static void main(String[] args) {
-        LinkedList tokens;
-        tokens = split(getMessage());
-        String result = join(tokens);
-        System.out.println(WordUtils.capitalize(result));
+        System.out.println("Hello World");
     }
 }
