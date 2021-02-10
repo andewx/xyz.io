ec285e666d3072dce85f16ff36084f8fae824284
Brian Anderson <brian.michael.anderson@gmail.com>
2021-02-11 00:17:58 +0900

    Update Server.java

--- webkit/src/main/java/xyz/Server/Server.java
+++ webkit/src/main/java/xyz/Server/Server.java
@@ -1,20 +1,14 @@
 package xyz.Server;
 
+import io.javalin.Javalin;
 
-import java.util.HashMap;
-
-//Server Interface - Hosts Application Protocols over HTTP Connection
-//Configures Server interface to be a functional handler. Attaches an endpoint to http request.
 public interface Server {
-
-    int httpInitConnection(HashMap<String,String> configuration);
-    int listenRequest(HashMap<String,String> listenProperties);
-    int preRequest(HttpCallback handler);
-    int postRequest(HttpCallback handler);
-    int preResponse(HttpCallback handler);
-    int postResponse(HttpCallback handler);
-    int httpResponse(HttpCallback handler);
-
+    public void loadConfig(String runtimeConfig);
+    public void addRoute(String path); 
+    public void startServer(int port);
+    public void get();
+    public void post();
+    public void response();
 }
 
 
